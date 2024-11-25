c0333052c62c2c6db42fa17edfc502dbb6f386c1
Adryan Henrique <hsadryan16@gmail.com>
2024-11-25 14:15:17 -0300

    chore: ajjusting cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { routes } from "./routes";
 import { config } from './utils/config';
+import cors from "cors"; 
 
 const app = express()
 const PORT = config.port;
+
+// Middleware para habilitar o CORS
+app.use(
+  cors({
+    origin: "http://localhost:4001",
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type"], // Cabeçalhos permitidos
+  })
+);
 
 app.use(express.json())
 
